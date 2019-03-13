dcc4d812150459c808ec2e4c8141b1bcd28c1a64
404status <fish.taixian@gmail.com>
2019-03-13 11:55:18 +0800

    提交分支

---
+++ app/Http/Controllers/Index/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+
+
+class IndexController
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications from
+    | your application to your users. Feel free to explore this trait.
+    |
+    */
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+    public function index(){
+        return view('index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('foo', function () {
+//     return 'Hello World';
+// });
+//个人信息设置
+
+Route::namespace('Index')->group(function () {
+    Route::any('index', 'IndexController@index');
+});
